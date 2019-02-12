5bc1c97b5fe3ee7b07fb894dd1889c4d9e4371f0
Nolan Allen <nolan.r.allen@gmail.com>
2019-02-12 15:16:45 -0600

    2nd commit; have login fields and buttons ready for functionality, functionality proving difficult

--- marketingRus/src/index.js
+++ marketingRus/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import App from './App';
+import App from './components/App';
 import rootReducer from './reducers';
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Router><Provider store={store}><App /></Provider></Router>, document.getElementById('root'));
---
+++ marketingRus/src/reducers/pitchReducer.js
@@ -0,0 +1,29 @@
+import { FETCH_ITEMS, FETCH_ITEMS_SUCESS } from '../actions';
+
+const iniState = {
+    items: [],
+    fetchingItems: false,
+    error: null
+}
+
+const pitchReducer = (state = iniState, action) => {
+    switch(action.type) {
+        case FETCH_ITEMS:
+            return {
+                ...state,
+                fetchingItems: true
+            };
+        case FETCH_ITEMS_SUCESS:
+            return {
+                ...state,
+                items: [
+                    ...action.results
+                ],
+                fetchingItems: false
+            }
+        default:
+            return state
+    }
+}
+
+export default pitchReducer;
